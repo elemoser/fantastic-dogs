bd5e230f1924e019e36bb6d64d9aa71fcb577d2f
“Elena <elemoser@gmail.com>
2024-08-22 17:04:08 +0200

    Create basic layout

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import { Avatar, HStack, Text } from "@chakra-ui/react";
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Avatar size="md" src="https://bit.ly/broken-link" />
+      <Text>NavBar</Text>
+    </HStack>
+  );
+};
+
+export default NavBar;
