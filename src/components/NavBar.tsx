a6e320fb21add04246084af79c4838825b88318b
“Elena <elemoser@gmail.com>
2024-08-22 17:26:01 +0200

    Create color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
 import { Avatar, HStack, Text } from "@chakra-ui/react";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="20px">
       <Avatar size="md" src="https://bit.ly/broken-link" />
-      <Text>NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
